da2c4f441ebbd73eb30dd8e159d394c3d9efec85
vivere-dally <stefanbuciujr@gmail.com>
2025-06-17 18:18:47 +0300

    trying stuff

--- extra-config.nix
+++ extra-config.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }:
 {
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+  hardware.enableRedistributableFirmware = true;
+  nixpkgs.hostPlatform = "aarch64-linux";
+
   networking.firewall.enable = false;
 
   nixpkgs.config.allowUnfree = true;
--- flake.nix
+++ flake.nix
@@ -9,43 +9,43 @@
   };
 
   outputs = { self, nixpkgs, nixos-generators, ... }:
-  {
+  let
+    # System-specific configurations
+    systems = {
+      x86_64-linux = "x86_64-linux";
+      aarch64-linux = "aarch64-linux";
+    };
+    # Cross-compilation setup for x86_64 -> aarch64
+    pkgsCross = pkgs: pkgs.pkgsCross.aarch64-multiplatform;
+  in {
     nixosModules = {
       system = {
-        disabledModules = [
-          "profiles/base.nix"
-        ];
-
+        disabledModules = [ "profiles/base.nix" ];
         system.stateVersion = "25.05";
-      };  
+      };
       users = {
-        users.users = {
-          admin = {
-            password = "admin1234";
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-          };
+        users.users.admin = {
+          password = "admin1234";
+          isNormalUser = true;
+          extraGroups = [ "wheel" ];
         };
-      };  
-    };  
+      };
+    };
 
-    packages.aarch64-linux = {
-      sdcard = 
-        let 
-          crossPkgs = import nixpkgs {
-            system      = "aarch64-linux";
-            crossSystem = { config = "aarch64-linux"; };
-          };
-        in
-        nixos-generators.nixosGenerate {
-          system = "aarch64-linux";
-          format = "sd-aarch64";
-          modules = [
-            ./extra-config.nix
-            self.nixosModules.system
-            self.nixosModules.users
-          ];
-        };
+    # Build for x86_64 host with cross-compilation
+    packages.x86_64-linux.sdcard = let
+      pkgs = import nixpkgs {
+        system = systems.x86_64-linux;
+        config.allowUnsupportedSystem = true;
+      };
+    in nixos-generators.nixosGenerate {
+      pkgs = pkgsCross pkgs;  # Cross-compile to aarch64
+      format = "sd-aarch64";
+      modules = [
+        ./extra-config.nix
+        self.nixosModules.system
+        self.nixosModules.users
+      ];
     };
   };
 }
